aed0e2077a09490cec5ace904f9d59caf71128c3
Brian-CodeNation <brian.harkins@wearecodenation.com>
2023-01-10 06:59:58 +0000

    Removed unecessary lines

--- snake.js
+++ snake.js
@@ -66,6 +66,7 @@ function drawGame() {
 }
 
 function isGameOver() {
+  
   let gameOver = false;
 
   if (yVelocity === 0 && xVelocity === 0) {
@@ -93,23 +94,17 @@ function isGameOver() {
 
   if (gameOver) {
     ctx.fillStyle = "white";
+    ctx.font = "50px Verdana";    
+    ctx.fillStyle = "white";
     ctx.font = "50px Verdana";
 
-    if (gameOver) {
-      ctx.fillStyle = "white";
-      ctx.font = "50px Verdana";
-
-      var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
-      gradient.addColorStop("0", " magenta");
-      gradient.addColorStop("0.5", "blue");
-      gradient.addColorStop("1.0", "red");
-      // Fill with gradient
-      ctx.fillStyle = gradient;
-
-      ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
-    }
-
-    ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+    var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    gradient.addColorStop("0", " magenta");
+    gradient.addColorStop("0.5", "blue");
+    gradient.addColorStop("1.0", "red");
+    // Fill with gradient
+    ctx.fillStyle = gradient;
+    ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);    
   }
 
   return gameOver;
